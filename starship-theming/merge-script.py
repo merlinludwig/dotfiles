9eea7768602dc609178e15add5ff66e2ac6bd962
Merlin Ludwig <merlin.ludwig@gmail.com>
2026-07-13 11:41:41 -0500

    new file:   starship-theming/extra.toml 	new file:   starship-theming/merge-script.py 	new file:   starship-theming/style-default.toml 	new file:   starship-theming/style-p10k.toml 	new file:   starship-theming/style.toml 	new file:   starship-theming/symbol-default.toml 	new file:   starship-theming/symbol.toml 	modified:   starship/.config/starship.toml 	modified:   termux/.termux/colors.properties 	modified:   termux/.termux/font.ttf 	modified:   zsh/.zshrc

---
+++ starship-theming/merge-script.py
@@ -0,0 +1,23 @@
+import tomlkit
+
+def deep_merge(base, overlay):
+    for key, value in overlay.items():
+        if key in base and isinstance(base[key], dict) and isinstance(value, dict):
+            deep_merge(base[key], value)
+        else:
+            base[key] = value
+    return base
+
+def merge_files(paths, output_path):
+    with open(paths[0]) as f:
+        merged = tomlkit.parse(f.read())
+
+    for path in paths[1:]:
+        with open(path) as f:
+            overlay = tomlkit.parse(f.read())
+        deep_merge(merged, overlay)
+
+    with open(output_path, "w") as f:
+        f.write(tomlkit.dumps(merged))
+
+merge_files(["style.toml", "symbol.toml", "extra.toml"], "../starship/.config/starship.toml")
